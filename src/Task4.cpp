41ad7a4a6343514819a89859756c0905db355bd2
Little8 <88222501+19ty02@users.noreply.github.com>
2024-06-02 16:46:53 +0800

    finish

--- src/Task4.cpp
+++ src/Task4.cpp
@@ -1,46 +1,54 @@
 #include<cstdio>
 #include<iostream>
 using namespace std;
-double d=0.1;
+const int d=10;  // ²»¾ùºâ¶È 1/d 
 int a[4]={0,11,11,13};
 int b[4]={0,6,6,5};
-double c[4]={0,175.6,194.9,210.5};
+int c[4]={0,1756,1949,2105};
 // x=t, y=T, z=1/v
 struct node{
-	double a,b;  // z >= ax+by
+	int c,a,b;  // cz >= ax + by
 	int sgn;     // 1: >= | -1: <=
+	void trans_sgn(){
+		if(c<0){
+			c=-c;
+			a=-a;
+			b=-b;
+			sgn=-sgn;
+		}
+	}
 	void prt(){
-		printf("u");
+		trans_sgn();
+		printf("%du",c);
 		if(sgn==1)printf(" >= ");
 		else printf(" <= ");
-		printf("%.5lft+%.5lfT",a,b);
+		printf("%dt + %dT",a,b);
 	}
 }; 
 
 void work(){
 	node ans;
-	if(a[1]!=a[2]){
-		if(c[1]<c[2])ans.sgn=1;
-		else ans.sgn=-1;
-		ans.a=(a[1]-a[2])/(c[2]-c[1]);
-		ans.b=(b[1]-b[2])/(c[2]-c[1]);
-		ans.prt();
-		printf(" and ");
-	}
-	
 	if(a[2]!=a[3]){
-		if(c[2]<c[3])ans.sgn=1;
-		else ans.sgn=-1;
-		ans.a=(a[2]-a[3])/(c[3]-c[2]);
-		ans.b=(b[2]-b[3])/(c[3]-c[2]);
+		ans.sgn=1;
+		ans.c=c[3]-c[2];
+		ans.a=10*(a[2]-a[3]);
+		ans.b=10*(b[2]-b[3]);
 		ans.prt();
 		printf(" and ");
 	}
 	
-	if(d*c[3]-c[3]+c[1]>0)ans.sgn=1;
-	else ans.sgn=-1;
-	ans.a=(a[3]-d*a[3]-a[1])/(d*c[3]-c[3]+c[1]);
-	ans.b=(b[3]-d*b[3]-b[1])/(d*c[3]-c[3]+c[1]);
+	if(a[1]!=a[2]){
+		ans.sgn=1;
+		ans.c=c[2]-c[1];
+		ans.a=10*(a[1]-a[2]);
+		ans.b=10*(b[1]-b[2]);
+		ans.prt();
+		printf(" and ");
+	}
+	ans.sgn=1;
+	ans.c=c[3]-d*c[3]+d*c[1];
+	ans.a=10*(d*a[3]-a[3]-d*a[1]);
+	ans.b=10*(d*b[3]-b[3]-d*b[1]);
 	ans.prt();
 }
 int main(){
